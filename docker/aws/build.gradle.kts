66dd70b03e5c44d305e6523fdf9de487dfe11e1e
Daniel Mason <dan@juxt.pro>
2023-11-21 14:25:43 +0000

    WIP: AWS docker image.

---
+++ docker/aws/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    java
+    application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":pgwire-server"))
+    implementation(project(":http-server"))
+    implementation(project(":modules:kafka"))
+    implementation(project(":modules:s3"))
+    implementation(project(":modules:flight-sql"))
+    implementation("ch.qos.logback", "logback-classic", "1.4.5")
+}
+
+application {
+    mainClass.set("clojure.main")
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("xtdb")
+    archiveVersion.set("")
+    archiveClassifier.set("aws-docker")
+    mergeServiceFiles()
+}
