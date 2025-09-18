20eb0371349631952dea91ff88ef9a5b5b447023
Daniel Mason <dan@juxt.pro>
2025-09-18 15:09:49 +0100

    Further TPCH testing of oomkills.

---
+++ dev/memory_usage_tpch.clj
@@ -0,0 +1,25 @@
+(ns memory-usage-tpch
+  (:require [next.jdbc :as jdbc]
+            [xtdb.datasets.tpch :as tpch]))
+
+(defn ->connection []
+  (jdbc/get-connection {:dbname "xtdb"
+                        :host "localhost"
+                        :port 5432
+                        :classname "xtdb.jdbc.Driver"
+                        :dbtype "xtdb"}))
+
+(defn submit-and-query-tpch []
+  ;; submit TPCH SF 0.5
+  (with-open [conn (->connection)]
+    (tpch/submit-dml-jdbc! conn 0.5))
+
+  ;; followed by this query:
+  (with-open [conn (->connection)]
+    (jdbc/execute! conn ["select count(*) from orders;"])))
+
+(comment
+  
+  (submit-and-query-tpch)
+  
+  )
